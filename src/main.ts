f130bbded570c220c71a8f9fcd220a2c123292a6
Anubizze <vldkryukov@gmail.com>
2026-02-23 23:47:23 +0300

    upd package.json

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
       }
     )
   )
-  await app.listen(process.env.PORT ?? 3001);
+  const port = process.env.PORT ?? 3001;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
